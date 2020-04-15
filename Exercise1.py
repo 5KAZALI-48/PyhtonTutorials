6b99073f5a4572765fbecf840b0b2d748af773e2
fkaragoz <fkaragoz@asu.edu>
2020-04-15 16:34:12 -0700

    Excercise1:Print your Birthday in xx/xx/xxxx or xx-xx-xxxx format using 1 / or -

---
+++ Exercise1.py
@@ -0,0 +1,10 @@
+import sys
+import math
+import random
+import threading
+import time
+from functools import reduce
+
+print("Hello World")
+print(12, 25, 1988, sep='/')
+print(12, 25, 1988, sep='-')
